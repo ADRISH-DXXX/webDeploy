ab239eb5fda409cc10946148caf1677f982e7e58
ADRISH_XX()XX <77157837+ADRISH-DXXX@users.noreply.github.com>
2023-11-20 11:50:03 +0530

    Add files via upload

---
+++ iq.js
@@ -0,0 +1,157 @@
+function increaseSizeMath()
+{
+    var zoom = document.getElementById("zoomMath");
+    zoom.style.width = "120px";
+    zoom.style.opacity ="2";
+   
+}
+
+function normalSizeMath()
+{
+    var zoom = document.getElementById("zoomMath");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeEnglish()
+{
+    var zoom = document.getElementById("zoomEnglish");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeEnglish()
+{
+    var zoom = document.getElementById("zoomEnglish");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeComputer()
+{
+    var zoom = document.getElementById("zoomComputer");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeComputer()
+{
+    var zoom = document.getElementById("zoomComputer");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizePhysics()
+{
+    var zoom = document.getElementById("zoomPhysics");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizePhysics()
+{
+    var zoom = document.getElementById("zoomPhysics");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeCommerce()
+{
+    var zoom = document.getElementById("zoomCommerce");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeCommerce()
+{
+    var zoom = document.getElementById("zoomCommerce");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeBussiness()
+{
+    var zoom = document.getElementById("zoomBussiness");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeBussiness()
+{
+    var zoom = document.getElementById("zoomBussiness");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeBiology()
+{
+    var zoom = document.getElementById("zoomBiology");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeBiology()
+{
+    var zoom = document.getElementById("zoomBiology");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeChemistry()
+{
+    var zoom = document.getElementById("zoomChemistry");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeChemistry()
+{
+    var zoom = document.getElementById("zoomChemistry");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeHisotry()
+{
+    var zoom = document.getElementById("zoomHisotry");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeHisotry()
+{
+    var zoom = document.getElementById("zoomHisotry");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeEconomics()
+{
+    var zoom = document.getElementById("zoomEconomics");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeEconomics()
+{
+    var zoom = document.getElementById("zoomEconomics");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeGeography()
+{
+    var zoom = document.getElementById("zoomGeography");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeGeography()
+{
+    var zoom = document.getElementById("zoomGeography");
+    zoom.style.width = "100px";
+   
+}
+function increaseSizeAccounts()
+{
+    var zoom = document.getElementById("zoomAccounts");
+    zoom.style.width = "120px";
+   
+}
+
+function normalSizeAccounts()
+{
+    var zoom = document.getElementById("zoomAccounts");
+    zoom.style.width = "100px";
+   
+}
